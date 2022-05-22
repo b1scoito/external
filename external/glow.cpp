b6f672b58a38776ed25e6ef55d29b72aa14ef883
biscoito <biscoito@riseup.net>
2022-05-22 06:24:30 -0300

    add mutex lock for glow

--- external/glow.cpp
+++ external/glow.cpp
@@ -75,7 +75,9 @@ void c_glow::run( keybind& keybd )
 					0.8f							// A
 				);
 
+				mutex.lock();
 				g_memory->write<sdk::structs::glow_object_t>( entity_glow_offset, glow ); // Set glow
+				mutex.unlock();
 			}
 
 			auto end = std::chrono::high_resolution_clock::now();
